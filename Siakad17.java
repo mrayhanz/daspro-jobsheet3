61f2c655536470e8651664f78ac8610133a86d56
muhammadrayhan11 <rayhanzmzmi03@gmail.com>
2024-09-12 08:13:02 +0700

    first commit

---
+++ Siakad17.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Siakad17{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String nama,nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        
+        System.out.print("Masukkan nama: ");
+        nama = sc.nextLine();
+        System.out.print("Masukkan NIM: ");
+        nim = sc.nextLine();
+        System.out.print("Masukkan Kelas: ");
+        kelas = sc.nextLine().charAt(0);
+        System.out.print("Masukkan nomor absen: ");
+        absen = sc.nextByte();
+        
+        System.out.print("Masukkan Nilai Kuis: ");
+        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukkan Nilai Tugas: ");
+        nilaiTugas = sc.nextDouble();
+        System.out.print("Masukkan Nilai Ujian: ");
+        nilaiUjian = sc.nextDouble();
+
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+
+        System.out.println("Nama: " + nama + " Nim: " + nim);
+        System.out.println("Kelas: " + kelas + " Absen: " + absen);
+        System.out.println("Nilai Akhir: " + nilaiAkhir);
+        }
+}
